7c72841b861ff97ded9b1c267fdc29f1f8fc6920
Chandru779 <chanducse2000@gmail.com>
2021-08-26 12:12:00 +0530

    I have replaced switch with HashRouter

---
+++ build/precache-manifest.76d20c266f23ee045df0ce92d2cdd8a0.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "91d1a92eba4a6328e179289f4d606fdf",
+    "url": "/Portfolio/index.html"
+  },
+  {
+    "revision": "0eefc351a3b7605b6346",
+    "url": "/Portfolio/static/css/main.50a3335d.chunk.css"
+  },
+  {
+    "revision": "93cdbba8df62ac9039e7",
+    "url": "/Portfolio/static/js/2.5fc35ecb.chunk.js"
+  },
+  {
+    "revision": "0eefc351a3b7605b6346",
+    "url": "/Portfolio/static/js/main.bb3d9c37.chunk.js"
+  },
+  {
+    "revision": "a218fe782f6b1aff2f5c",
+    "url": "/Portfolio/static/js/runtime-main.95dee7a4.js"
+  },
+  {
+    "revision": "0236293412ce637f4fa23481c44e4d70",
+    "url": "/Portfolio/static/media/chandu1.02362934.jpeg"
+  },
+  {
+    "revision": "3521924118f3b7f7818c80030b3a4f8c",
+    "url": "/Portfolio/static/media/chandu2.35219241.jpeg"
+  }
+]);
