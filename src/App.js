b56200a2b43367e17258f7def4a1a74d35a513cd
Chandru Angadi <83079815+Chandru779@users.noreply.github.com>
2023-03-30 22:01:39 +0530

    update with demo code

--- src/App.js
+++ src/App.js
@@ -1,42 +1,10 @@
 import React from 'react';
 import './App.css';
-import NavMenu from './components/NavMenu';
-import { BrowserRouter,Route, Switch } from 'react-router-dom';
-import Home from './Pages/Home';
-import Project from './Pages/Project';
-import About from './Pages/About';
-import Contact from './Pages/Contact';
-
-
-
-
 
 function App() {
   return (
-    <div className="App">
-     
-     <BrowserRouter>
-        <NavMenu/>
-        
-        <Switch>
-          <Route path="/About">
-            <About />
-          </Route>
-
-          <Route path="/Project">
-           <Project />
-          </Route>
-
-          <Route path="/Contact"> 
-           <Contact />   
-          </Route>
-
-          <Route path="/"> 
-            <Home />  
-          </Route>
-        </Switch>
-
-     </BrowserRouter>
+    <div>
+     <h2>Chandru Portfolio </h2>
     </div>
   );
 }
